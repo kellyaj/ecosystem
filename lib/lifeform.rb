4c3ce40fbb85dea7c3c5ecce8639d4dc9e7c716e
Andrew Kelly <kellyaj@gmail.com>
2013-02-02 21:18:26 -0600

    LifeForms dying of old age

--- lib/lifeform.rb
+++ lib/lifeform.rb
@@ -1,6 +1,6 @@
 class LifeForm
 
-  attr_accessor :age
+  attr_accessor :age, :lifespan, :position, :world
 
   def initialize
     @age = 0
@@ -8,6 +8,15 @@ class LifeForm
 
   def cycle
     @age += 1
+    old_age_check
+  end
+
+  def old_age_check
+    self.kill if @age == @lifespan
+  end
+
+  def kill
+    @world.lifeforms.delete self
   end
 
 end
--- spec/lifeform_spec.rb
+++ spec/lifeform_spec.rb
@@ -6,6 +6,16 @@ describe "LifeForm" do
     it "should be born with an age of 0" do
       life.age.should == 0
     end
+
+    it "should have a life span" do
+      life.lifespan = 100
+      life.lifespan.should == 100
+    end
+
+    it "should spawn at a specific position" do
+      life.position = 1
+      life.position.should == 1
+    end
   end
 
   describe "#cycling" do
@@ -16,8 +26,16 @@ describe "LifeForm" do
   end
 
   describe "#death" do
-    xit "should die when it reaches old age" do
-      life.age = 100
+    it "should die when it reaches old age" do
+      world = World.new(10)
+      life.world = world
+      life.position = 1
+      world.add_lifeform(life)
+      life.lifespan = 100
+      100.times do
+        life.cycle
+      end
+      life.age.should == 100
     end
   end
 
